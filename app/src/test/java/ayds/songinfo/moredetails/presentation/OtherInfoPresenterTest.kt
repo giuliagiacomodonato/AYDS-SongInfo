05ee7e58e3641ca63c5a33767b4dde99d75d0d85
Giulia <giuliagiacomodonato1@gmail.com>
2025-05-28 11:15:08 -0300

    -- fix test otherinfopresenter

--- app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -1,32 +1,31 @@
 package ayds.songinfo.moredetails.presentation
 
-import ayds.songinfo.moredetails.data.external.OtherInfoService
-import ayds.songinfo.moredetails.data.local.OtherInfoLocalStorage
+
 import ayds.songinfo.moredetails.domain.ArtistBiography
 import ayds.songinfo.moredetails.domain.OtherInfoRepository
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.Assert
 import org.junit.Test
 
 class OtherInfoPresenterTest {
+
     private val otherInfoRepository: OtherInfoRepository = mockk()
     private val artistBiographyDescriptionHelper: ArtistBiographyDescriptionHelper = mockk()
-    private val otherInfoPresenter: OtherInfoPresenter = OtherInfoPresenterImpl(otherInfoRepository, artistBiographyDescriptionHelper)
+    private val otherInfoPresenter: OtherInfoPresenter =
+        OtherInfoPresenterImpl(otherInfoRepository, artistBiographyDescriptionHelper)
 
     @Test
-    fun `getArtistInfo should return artist biography ui state`(){
-        val artistBiography = ArtistBiography("artist", "biography", "url")
-        every{ otherInfoRepository.getArtistInfo("artist") } returns artistBiography
-        every{artistBiographyDescriptionHelper.getDescription(artistBiography)} returns "description"
+    fun `getArtistInfo should return artist biography ui state`() {
+        val artistBiography = ArtistBiography("artistName", "biography", "articleUrl")
+        every { otherInfoRepository.getArtistInfo("artistName") } returns artistBiography
+        every { artistBiographyDescriptionHelper.getDescription(artistBiography) } returns "description"
         val artistBiographyTester: (ArtistBiographyUiState) -> Unit = mockk(relaxed = true)
 
         otherInfoPresenter.artistBiographyObservable.subscribe(artistBiographyTester)
+        otherInfoPresenter.getArtistInfo("artistName")
 
-        otherInfoPresenter.getArtistInfo("artist")
-        val expectedValue = ArtistBiographyUiState("artist", "biography", "url")
-        verify { artistBiographyTester(expectedValue) }
-
+        val result = ArtistBiographyUiState("artistName", "description", "articleUrl")
+        verify { artistBiographyTester(result) }
     }
 }
